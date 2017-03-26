157a150acf07e869bcccf75929af4588e2332798
delvison castillo <delvisoncastillo@gmail.com>
2017-03-26 19:17:23 -0400

    node api tested. view created under route /whoshome

--- connect_alert.py
+++ connect_alert.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python2.7
-# This script requires asound and espeak be installed in the system
 
 import time
 import datetime
@@ -9,6 +8,8 @@ import select
 import subprocess
 import json
 from pprint import pprint
+
+# foreign libraries to install for python2.7
 import argparse
 from scapy.all import *
 import pyttsx
@@ -17,71 +18,86 @@ import requests
 # from urllib.request import Request, urlopen
 # from twilio.rest import TwilioRestClient
 
+# terminal colors
 OKGREEN = '\033[92m'
 FAIL = '\033[91m'
 ENDC = '\033[0m'
-ERROR = FAIL+"ERROR: "+ENDC
+ERROR = FAIL + "ERROR: " + ENDC
 OKBLUE = '\033[94m'
 
-connected = {} # { "MAC": "time connected"}
-mac_defs = {} #  { "MAC": "device name"}
-black_list = [] # MAC addresses to exclude from monitoring
-disconnect_time = 1800 # time to wait before client is rendered disconnected
-log_file = "unknown_connections.log" # log file for unkown devices
-connected_file = "connected_file.json"
-__checking = False
-__broadcast = True
-_speak = False # flag for speaking when a new device connects
-web_service_url = "http://192.168.1.50/api/whoshome"
+connected = {}  # { "MAC": "time connected"}
+mac_defs = {}  # { "MAC": "device name"}
+black_list = []  # MAC addresses to exclude from monitoring
+
+loop_seconds = 30  # seconds to wait in between each monitor loop
+disconnect_time = 1800  # time to wait before client is rendered disconnected
+__checking = False  # flag that indicates whether or not to notice new connections
+__broadcast = True  # toggle for broadcasting to a web service
+__writing = False  # toggle for writing connections to a file
+__speak = False  # flag for speaking when a new device connects
+__log_unknown = True  # flag to indicate whether to log unknown devices
+connected_file = "connected_file.json"  # file to log connections
+log_file = "unknown_connections.log"  # log file for unkown devices
+web_service_url = "http://192.168.1.50:3000/whoshome"  # web service url
+
 
 def speak(say_this):
-  global _speak
-  if _speak:
-    os.system('aplay blip.wav -R 1')
-    speech_engine = pyttsx.init()
-    speech_engine.say(say_this)
-    speech_engine.runAndWait()
+    """
+    Will speak the given message
+    """
+    global __speak
+    if __speak:
+        os.system('aplay blip.wav -R 1')
+        speech_engine = pyttsx.init()
+        speech_engine.say(say_this)
+        speech_engine.runAndWait()
+
 
 def get_lan_ip():
-  """
-  Gets the current LAN's IP by connecting to google and extracting the IP from
-  the socket.
-  """
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  s.connect(("google.com", 80))
-  ip = s.getsockname()
-  s.close()
-  return ip[0]
+    """
+    Gets the current LAN's IP by connecting to google and extracting the IP from
+    the socket.
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("google.com", 80))
+    ip = s.getsockname()
+    s.close()
+    return ip[0]
+
 
 def get_gateway_ip(gateway_ip=None):
-  # Gets the gateway ip. Returns [gateway_ip, ip_range]
-  myip = get_lan_ip()
-  ip_list = myip.split('.')
-  del ip_list[-1]
-  ip_list.append('*')
-  ip_range = '.'.join(ip_list)
-  del ip_list[-1]
-  ip_list.append('1')# assumed default gateway is "XXX.XXX.XXX.1"
-  if gateway_ip is not None:
-    # print "Gateway IP provided as "+gateway_ip
-    return [gateway_ip, ip_range]
-  else:
-    # print "Gateway IP provided as "+'.'.join(ip_list)
-    return ['.'.join(ip_list), ip_range]
+    """
+    Gets the gateway ip. Returns [gateway_ip, ip_range]
+    """
+    myip = get_lan_ip()
+    ip_list = myip.split('.')
+    del ip_list[-1]
+    ip_list.append('*')
+    ip_range = '.'.join(ip_list)
+    del ip_list[-1]
+    ip_list.append('1')  # assumed default gateway is "XXX.XXX.XXX.1"
+    if gateway_ip is not None:
+        # print "Gateway IP provided as "+gateway_ip
+        return [gateway_ip, ip_range]
+    else:
+        # print "Gateway IP provided as "+'.'.join(ip_list)
+        return ['.'.join(ip_list), ip_range]
+
 
 def get_ip_macs(ips):
-  """
-  Returns a list of tupples containing the (ip, mac address)
-  of all of the computers on the network
-  """
-  answers, uans = arping(ips, verbose=0)
-  res = []
-  for answer in answers:
-    mac = answer[1].hwsrc
-    ip  = answer[1].psrc
-    res.append((ip, mac))
-    # res.append(specific_target)
-  return res
+    """
+    Returns a list of tupples containing the (ip, mac address)
+    of all of the computers on the network
+    """
+    answers, uans = arping(ips, verbose=0)
+    res = []
+    for answer in answers:
+        mac = answer[1].hwsrc
+        ip = answer[1].psrc
+        res.append((ip, mac))
+        # res.append(specific_target)
+    return res
+
 
 def load_macs(filename):
     """
@@ -93,50 +109,59 @@ def load_macs(filename):
     for each in data:
         mac_defs[each['MAC'].lower()] = each['device']
 
-def check_connected(gateway_ip,ip_range):
-    global mac_defs, connected,__checking, black_list
+
+def check_connected(gateway_ip, ip_range):
+    global mac_defs, connected, __checking, black_list
     __new_connection = False
     devices = get_ip_macs(ip_range)
-    subprocess.call('clear', shell=True) # clear the shell
+    subprocess.call('clear', shell=True)  # clear the shell
     i = 0
     for device in devices:
         if device[1] not in black_list:
-           if device[1] in mac_defs.keys():
-               print '%s)\t%s\t%s\t%s%s' % (i, device[0],device[1],mac_defs[device[1]],ENDC)
-               if device[1] not in connected.keys():
-                   __new_connection = True
-                   # notify
-                   if __checking:
-                       print OKGREEN+mac_defs[device[1]] + " just connected!"+ENDC
-                       speak(mac_defs[device[1]]+"\" ")
-                # update time stamp
-               connected[device[1]] = datetime.now()
-           else:
-            # unkown device
-               print '%s)\t%s\t%s' % (i, device[0], device[1] )
-               speak("foreign device detected")
-               log_foreign_access(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+" "+device[1])
-               mac_defs[device[1]] = "UNKNOWN_DEVICE"
-               connected[device[1]] = datetime.now()
+            if device[1] in mac_defs.keys():
+                print '%s)\t%s\t%s\t%s%s' % (i, device[0], device[1], mac_defs[device[1]], ENDC)
+                if device[1] not in connected.keys():
+                    __new_connection = True
+                    # notify
+                    if __checking:
+                        print OKGREEN + mac_defs[device[1]] + " just connected!" + ENDC
+                        speak(mac_defs[device[1]] + "\" ")
+                 # update time stamp
+                connected[device[1]] = datetime.now()
+            else:
+             # unkown device
+                print '%s)\t%s\t%s' % (i, device[0], device[1])
+                speak("foreign device detected")
+                log_foreign_access(datetime.now().strftime(
+                    "%Y-%m-%d %H:%M:%S") + " " + device[1])
+                mac_defs[device[1]] = "UNKNOWN_DEVICE"
+                connected[device[1]] = datetime.now()
 
-           i+=1
-        if __broadcast:
-           broadcast_connection(device[1], mac_defs[device[1]], connected[device[1]])
-    __checking =True
+            i += 1
+            if __broadcast:
+                broadcast_connection(device[1], mac_defs[
+                                     device[1]], connected[device[1]])
+    __checking = True
     return __new_connection
 
+
 def monitor_loop(gateway_ip, ip_range):
+    """
+    main monitor loop of the program. checks for new connections and
+    disconnections.
+    """
+    global loop_seconds
     while True:
         try:
             new_connections = check_connected(gateway_ip, ip_range)
             new_disconnections = check_for_disconnections()
-            # TODO: write to file those devices which are currently connected
             if new_connections or new_disconnections:
                 write_connections()
-            time.sleep(30)
+            time.sleep(loop_seconds)
         except KeyboardInterrupt:
             print "shutting down..."
             sys.exit()
+
 
 def check_for_disconnections():
     """
@@ -149,17 +174,18 @@ def check_for_disconnections():
     for each in connected:
         diff = datetime.now() - connected[each]
         diff = diff.total_seconds()
-        print(OKBLUE +"%20s%20s"+ENDC) % (mac_defs[each], str(diff))
+        print(OKBLUE + "%20s%20s" + ENDC) % (mac_defs[each], str(diff))
         if diff >= disconnect_time:
             to_be_removed.append(each)
             __new_disconnection = True
     for each in to_be_removed:
-        connected.pop(each)
+        t = connected.pop(each)
         if __broadcast:
-            broadcast_disconnection(each["MAC"])
-        print FAIL+mac_defs[each] +" disconnected "+ENDC
+            broadcast_disconnection(t)
+        print FAIL + mac_defs[each] + " disconnected " + ENDC
 
     return __new_disconnection
+
 
 def load_blacklist(filename):
     """
@@ -167,29 +193,36 @@ def load_blacklist(filename):
     the text file should be 1 MAC address per line
     """
     global black_list
-    print "...loading blacklist from "+filename
     count = 0
     with open(filename) as data_file:
         for each in data_file:
             black_list.append(each.strip())
-            count = count +1
-    print "...loaded "+str(count)+" from "+filename
+            count = count + 1
 
 
 def broadcast_connection(mac, alias, timestamp):
+    """
+    sends a http POST request with the MAC address of a newly connected
+    device to a specified url
+    """
     try:
         global web_service_url
-        post_fields = {"mac":mac,"alias":alias,"timestamp":timestamp}
+        post_fields = {"mac": mac, "alias": alias, "timestamp": timestamp}
         requests.post(web_service_url, data=post_fields)
         # request = Request(web_service_url, urlencode(post_fields).encode())
         # res = json.loads(urlopen(request).read().decode())
     except requests.exceptions.ConnectionError:
         pass
 
+
 def broadcast_disconnection(mac):
+    """
+    sends a http DELETE request with the MAC address of a newly disconnected
+    device to a specified url
+    """
     try:
         global web_service_url
-        post_fields = {"mac":mac}
+        post_fields = {"mac": mac}
         requests.delete(web_service_url, data=post_fields)
         # request = Request(web_service_url+"/"+str(order_id), method='DELETE')
         # res = json.loads(urlopen(request).read().decode())
@@ -202,18 +235,24 @@ def broadcast_disconnection(mac):
 #                        to='+15162590083',
 #                        body=msg)
 
+
 def write_connections():
-    global connected, mac_defs
-    data = {}
-    data['connections'] = []
-    for mac in connected:
-        obj = {}
-        obj['MAC'] = mac
-        obj['alias'] = mac_defs[mac]
-        obj['timestamp'] = str(connected[mac])
-        # data = data + mac + ", "+mac_defs[mac]+", " +str(connected[mac])+ "\n"
-        data['connections'].append(obj)
-    write_to_file(connected_file, json.dumps(data, indent=4), "w")
+    """
+    writes out connections to a JSON file
+    """
+    global connected, mac_defs, __writing
+    if __writing:
+        data = {}
+        data['connections'] = []
+        for mac in connected:
+            obj = {}
+            obj['MAC'] = mac
+            obj['alias'] = mac_defs[mac]
+            obj['timestamp'] = str(connected[mac])
+            # data = data + mac + ", "+mac_defs[mac]+", " +str(connected[mac])+ "\n"
+            data['connections'].append(obj)
+        write_to_file(connected_file, json.dumps(data, indent=4), "w")
+
 
 def log_foreign_access(entry):
     """
@@ -223,28 +262,44 @@ def log_foreign_access(entry):
     # create file if not exists
     write_to_file(log_file, entry, "a")
 
+
 def write_to_file(file, data, write_option):
-    print "...writing to " + file
+    """
+    writes data to a file.
+    """
     if not os.path.exists(file):
         open(file, 'w').close()
     with open(file, write_option) as myfile:
         os.chmod(file, 0777)
-        myfile.write(data+'\n')
+        myfile.write(data + '\n')
 
 
-if __name__=="__main__":
-    gateway_ip = None
-    ip_range = None
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-g", help="specify a gateway. default gateway will be 192.168.1.1") #gateway
-    parser.add_argument("-f", help='specify a JSON file with device names and their corresponding MAC addresses. Format should be [{"device": "some device", "MAC":"some mac"}]. default will be mac_addresses.json') #file with mac addresses
-    parser.add_argument("-e", help='specify a text file containing MAC addresses to exclude from monitoring. text file must have one MAC address per line. default file will be blacklist.txt') #text file with mac address exclusions
-    # parser.add_argument("-t", help="time to wait before a given client is considered disconnected. (seconds)") #time to wait before client is considered discon.
+    parser.add_argument(
+        "-g", help="specify a gateway. default gateway will be 192.168.1.1")  # gateway
+    # file with mac addresses
+    parser.add_argument(
+        "-f", help='specify a JSON file with device names and their corresponding MAC addresses. Format should be [{"device": "some device", "MAC":"some mac"}]. default will be mac_addresses.json')
+    # text file with mac address exclusions
+    parser.add_argument(
+        "-e", help='specify a text file containing MAC addresses to exclude from monitoring. text file must have one MAC address per line. default file will be blacklist.txt')
+    # parser.add_argument("-t", help="time to wait before a given client is
+    # considered disconnected. (seconds)") #time to wait before client is
+    # considered discon.
     args = parser.parse_args()
+
+    # check for root access
+    if os.getuid() is not 0:
+        print "This script requires root access."
+        sys.exit(1)
+
+    ip_range = None
+    gateway_ip = None
 
     if args.g is not None:
         gateway_ip = args.g
-        ip_range = ".".join(gateway_ip.split('.')[0:-1]) + '.'+'*'
+        ip_range = ".".join(gateway_ip.split('.')[0:-1]) + '.' + '*'
     else:
         gateway_ip, ip_range = get_gateway_ip()
     if args.f is not None:
@@ -257,4 +312,4 @@ if __name__=="__main__":
     #     global disconnect_time
     #     disconnect_time = args.t
 
-    monitor_loop(gateway_ip,ip_range)
+    monitor_loop(gateway_ip, ip_range)
--- node_app/index.js
+++ node_app/index.js
@@ -1,22 +1,32 @@
 
 
+var connections_file = "../connected_file.json"
 var bodyParser = require('body-parser');
 var express = require('express')
   , stylus = require('stylus')
   , nib = require('nib')
 var fs = require('fs')
 var app = express();
+
+// function compile(str, path) {
+//   return stylus(str)
+//     .set('filename', path)
+//     .use(nib());
+// }
+
+
 app.set('view engine', 'pug')
-
-
-var connections_file = "../connected_file.json"
-
-// setup bodyparser
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.use(bodyParser.json());
+app.use(express.static(__dirname+ '/stylesheets/'));
+// app.use(stylus.middleware(
+//   { src: __dirname+"/stylesheets"
+//   , dest: __dirname+"/stylesheets"
+//   , compile: compile
+//   }
+// ))
 
 // set up port
 var port = process.env.PORT || 3000;
@@ -25,18 +35,19 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    // console.log("+ " + req.ip + " - " + req.headers['user-agent']);
-    next(); // make sure we go to the next routes and don't stop here
-});
+// router.use(function(req, res, next) {
+//     // do logging
+//     // console.log("+ " + req.ip + " - " + req.headers['user-agent']);
+//     next(); // make sure we go to the next routes and don't stop here
+// });
 
 // test route (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({
-        message: 'its working!'
-    });
-});
+// router.get('/', function(req, res) {
+//     res.json({
+//         message: 'its working!'
+//     });
+// });
+router.route('/')
 
 router.route('/whoshome')
 
@@ -54,21 +65,27 @@ router.route('/whoshome')
             tmp.timestamp = timestamp
             var obj = JSON.parse(data)
             // TODO: debug for async flow
-            for (var i = 0; i < obj.connections.length; i++) {
+            // data = JSON.stringify(obj, null, 4)
+            check_exist(obj,write)
+            // write()
+            // write back to file
+            function write(x) {
+              fs.writeFile(connections_file, x, function(err) {
+                if (err) throw err
+                res.json(obj)
+              })
+            }
+            function check_exist(obj, callback) {
+              for (var i = 0; i < obj.connections.length; i++) {
                 var connection = obj.connections[i]
                 // console.log(connection.MAC);
                 if (connection.MAC === tmp.MAC) {
-                    obj.connections.splice(i, i)
-                    write(JSON.stringify(obj, null, 4))
+                  obj.connections.splice(i, 1)
                 }
+              }
+              obj.connections.push(tmp)
+              callback(JSON.stringify(obj, null, 4))
             }
-            obj.connections.push(tmp)
-            data = JSON.stringify(obj, null, 4)
-            // write back to file
-            fs.writeFile(connections_file, data, function(err) {
-                if (err) throw err
-                res.json(obj)
-            })
         })
     })
 
@@ -85,7 +102,7 @@ router.route('/whoshome')
                 var connection = obj.connections[i]
                 // console.log(connection.MAC);
                 if (connection.MAC === mac) {
-                    obj.connections.splice(i, i)
+                    obj.connections = obj.connections.splice(i, 1)
                     write(JSON.stringify(obj, null, 4))
                 }
             }
@@ -114,7 +131,7 @@ router.route('/whoshome')
 
 
 // REGISTER OUR ROUTES. all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/', router);
 
 // checks if json obj is empty
 function isEmpty(obj) {
